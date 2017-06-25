4bb9f9eb31dc916e7416d35fd4c692448721258c
ashar665 <ashar665@92.242.140.2>
2017-06-25 10:22:02 -0700

    initial build

---
+++ testJenkins/src/main/java/testJenkins/TestSample.java
@@ -0,0 +1,14 @@
+package testJenkins;
+
+public class TestSample {
+
+	private String testMe;
+
+	public String getTestMe() {
+		return testMe;
+	}
+
+	public void setTestMe(String testMe) {
+		this.testMe = testMe;
+	}
+}
