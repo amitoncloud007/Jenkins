c31f9d88586d4e187785d56a5b0bedc9b21bc2c9
ashar665 <ashar665@92.242.140.2>
2017-06-25 12:02:28 -0700

    4

--- testJenkins/src/main/java/testJenkins/TestSample.java
+++ testJenkins/src/main/java/testJenkins/TestSample.java
@@ -4,10 +4,18 @@ public class TestSample {
 
 	private String testMe;
 
+	/**
+	 * Sample Java Docs
+	 * @return
+	 */
 	public String getTestMe() {
 		return testMe;
 	}
 
+	/**
+	 * Sample Java docs
+	 * @param testMe
+	 */
 	public void setTestMe(String testMe) {
 		this.testMe = testMe;
 	}
